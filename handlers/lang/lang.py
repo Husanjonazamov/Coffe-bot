1868d3b89dc8ea8015c5a23abca4e6f71351e52e
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-10-08 15:50:36 +0500

    New g4f version: 0.6.3.6 (current: 0.3.9.7) | pip install -U g4f

--- handlers/lang/lang.py
+++ handlers/lang/lang.py
@@ -7,6 +7,7 @@ from loader import dp, bot
 from utils import texts, buttons
 from services.services import getProduct, patchUser, getUser
 from state.state import LangUpdate, CoffeState
+from handlers.start import start_handler
 
 
 async def _task(message: Message, state: FSMContext):
@@ -27,21 +28,9 @@ async def _task(message: Message, state: FSMContext):
 
     lang = lang_codes[lang_text]
 
-    first_name = message.from_user.first_name
     resoult = patchUser(tg_id=user_id,lang=lang)
-    
-    user_id = message.from_user.id
-    user = getUser(user_id)
-    lang = user['lang']
 
-    product = getProduct(lang)
-    
-    await message.answer(
-        texts.START[lang],
-        reply_markup=buttons.get_product(product, lang)    
-    )
-
-    await state.finish()
+    await start_handler(message, state)
 
 @dp.message_handler(content_types=['text'], state=LangUpdate.lang)
 async def lang_handler(message: Message, state: FSMContext):
